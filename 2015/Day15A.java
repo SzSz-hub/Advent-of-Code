c1791caa21c5c5bde2922afe17d0b64b0961bc84
SzSz-hub <szabolcs.szoke.szsz@gmail.com>
2026-02-06 15:57:04 +0100

    Update Day15A.java
    fixed class name

--- 2015/Day15A.java
+++ 2015/Day15A.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Day15 {
+public class Day15A {
     public static void main(String[] args) {
         final int TOTAL = 100;
         List<Ingredient> ingredients = new ArrayList<>();
